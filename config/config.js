be6f1a57a99c706997dc9b6a7d074750901a0543
sushil50889 <mandi.sushil@gmail.com>
2018-06-28 11:14:05 +0530

    final tested

--- config/config.js
+++ config/config.js
@@ -2,6 +2,6 @@ module.exports =
 {
     databaseMlab: "mongodb://mandisushil306:mandisushil306@ds113935.mlab.com:13935/mean-auth-test",
     secret: "passportsecret",
-    senderMail: 'testpurpose28@yahoo.com',
-    senderMailPass: 'Np1p#Mx9TI81UNN'
+    senderMail: process.env.EMAILID,
+    senderMailPass: process.env.EMAILPASS
 }
